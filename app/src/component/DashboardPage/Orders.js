9f435a1876b5f64eaf3819defc6f6827eae3d5b7
rahul <amit71instep@gmail.com>
2023-08-21 12:46:15 +0530

    order, signin,signup css

--- app/src/component/DashboardPage/Orders.js
+++ app/src/component/DashboardPage/Orders.js
@@ -86,128 +86,165 @@ const Orders = () => {
   ];
   return (
     <>
-      <Row>
-        <Col className="Admin_dashboard margin_bottom" lg={12}>
-          <h3> Orders</h3>
-        </Col>
-      </Row>
-      <Row>
-        <Col lg={12} className="searchbutton">
-          <div className="form_control_or_btngroup">
-            <div className="all_product_search">
-              <div className="oders_store"><h5>#I-Store</h5> </div>
-              <Form.Control
-                type="text"
-                placeholder="Search"
-                className=" mr-sm-2 adminsearch_bar"
-              />
+      <div className="admin_toppadding ">
+        <Row>
+          <Col className="Admin_dashboard margin_bottom" lg={12}>
+            <h3> Orders</h3>
+          </Col>
+        </Row>
+        <Row>
+          <Col lg={12} className="searchbutton">
+            <div className="form_control_or_btngroup row">
+              <div className="orders_Search col-lg-3 col-xl-3">
+                <Form.Control
+                  type="text"
+                  placeholder="Search"
+                  className=" mr-sm-2 adminsearch_bar"
+                />
+              </div>
+              <div className="orders_fivebutton col-lg-9 col-xl-9">
+                <Dropdown>
+                  <Dropdown.Toggle
+                    className="select_button"
+                    variant="success"
+                    id="dropdown-basic"
+                  >
+                    Payment Status
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu>
+                    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">
+                      Another action
+                    </Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">
+                      Something else
+                    </Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+                <Dropdown>
+                  <Dropdown.Toggle
+                    className="select_button"
+                    variant="success"
+                    id="dropdown-basic"
+                  >
+                    Delivery Status
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu>
+                    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">
+                      Another action
+                    </Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">
+                      Something else
+                    </Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+                <Dropdown>
+                  <Dropdown.Toggle
+                    className="select_button"
+                    variant="success"
+                    id="dropdown-basic"
+                  >
+                    Location
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu>
+                    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">
+                      Another action
+                    </Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">
+                      Something else
+                    </Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+                <Dropdown>
+                  <Dropdown.Toggle
+                    className="select_button"
+                    variant="success"
+                    id="dropdown-basic"
+                  >
+                    Online Orders
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu>
+                    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">
+                      Another action
+                    </Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">
+                      Something else
+                    </Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+                <Button className="select_button m-0" type="submit">
+                  {" "}
+                  <AiOutlineSearch /> search
+                </Button>
+              </div>
             </div>
-            <div className="btngroup">
-              <Dropdown>
-                <Dropdown.Toggle
-                  className="select_button"
-                  variant="success"
-                  id="dropdown-basic"
-                >
-                  Select Brand
-                </Dropdown.Toggle>
-                <Dropdown.Menu>
-                  <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                  <Dropdown.Item href="#/action-2">
-                    Another action
-                  </Dropdown.Item>
-                  <Dropdown.Item href="#/action-3">
-                    Something else
-                  </Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
-              <Dropdown>
-                <Dropdown.Toggle
-                  className="select_button"
-                  variant="success"
-                  id="dropdown-basic"
-                >
-                  Select Status
-                </Dropdown.Toggle>
-                <Dropdown.Menu>
-                  <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                  <Dropdown.Item href="#/action-2">
-                    Another action
-                  </Dropdown.Item>
-                  <Dropdown.Item href="#/action-3">
-                    Something else
-                  </Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
-              <Button className="select_button m-0" type="submit">
-                {" "}
-                <AiOutlineSearch /> search
-              </Button>
-            </div>
-          </div>
-          <Table responsive="md">
-            <thead>
-              <tr>
-                <th>S/L</th>
-                <th>Order Code</th>
-                <th>Customer</th>
-                <th>Placed On</th>
-                <th>Items</th>
-                <th>Payment</th>
-                <th>Status</th>
-                <th>Type</th>
-                <th>Location</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              {data?.map((item, index) => {
-                return (
-                  <tr key={index}>
-                    <td>{item?.sl}</td>
-                    <td>{item?.orderCode}</td>
-                    <td>
-                      <div className="d-flex">
-                        <img
-                          className="order_img me-2"
-                          src={item?.customerImage}
-                          alt="#"
-                        />
-                        <div>
-                          <p>{item?.customerName}</p>
-                          <p>{item?.customerContact}</p>
+            <Table responsive="md">
+              <thead>
+                <tr>
+                  <th>S/L</th>
+                  <th>Order Code</th>
+                  <th>Customer</th>
+                  <th>Placed On</th>
+                  <th>Items</th>
+                  <th>Payment</th>
+                  <th>Status</th>
+                  <th>Type</th>
+                  <th>Location</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data?.map((item, index) => {
+                  return (
+                    <tr key={index}>
+                      <td>{item?.sl}</td>
+                      <td>{item?.orderCode}</td>
+                      <td>
+                        <div className="d-flex">
+                          <img
+                            className="order_img me-2"
+                            src={item?.customerImage}
+                            alt="#"
+                          />
+                          <div>
+                            <p>{item?.customerName}</p>
+                            <p>{item?.customerContact}</p>
+                          </div>
                         </div>
-                      </div>
-                    </td>
-                    <td>{item?.placedOn}</td>
-                    <td>{item?.items}</td>
-                    <td>{item?.paymentStatus}</td>
-                    <td>{item?.status}</td>
-                    <td>{item?.type}</td>
-                    <td>{item?.location}</td>
-                    <td className=" ">
-                      <AiFillEye className="order_icon" />
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </Table>
-          <div className="table_pageination">
-            <Pagination>
-              <Pagination.Prev />
-              <Pagination.Item>{1}</Pagination.Item>
-              <Pagination.Item>{2}</Pagination.Item>
-              <Pagination.Item>{3}</Pagination.Item>
-              <Pagination.Item>{4}</Pagination.Item>
-              <Pagination.Item disabled>{5}</Pagination.Item>
-              <Pagination.Ellipsis />
-              <Pagination.Item>{20}</Pagination.Item>
-              <Pagination.Next />
-            </Pagination>
-          </div>
-        </Col>
-      </Row>
+                      </td>
+                      <td>{item?.placedOn}</td>
+                      <td>{item?.items}</td>
+                      <td>{item?.paymentStatus}</td>
+                      <td>{item?.status}</td>
+                      <td>{item?.type}</td>
+                      <td>{item?.location}</td>
+                      <td className=" ">
+                        <AiFillEye className="order_icon" />
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </Table>
+            <div className="table_pageination">
+              <Pagination>
+                <Pagination.Prev />
+                <Pagination.Item>{1}</Pagination.Item>
+                <Pagination.Item>{2}</Pagination.Item>
+                <Pagination.Item>{3}</Pagination.Item>
+                <Pagination.Item>{4}</Pagination.Item>
+                <Pagination.Item disabled>{5}</Pagination.Item>
+                <Pagination.Ellipsis />
+                <Pagination.Item>{20}</Pagination.Item>
+                <Pagination.Next />
+              </Pagination>
+            </div>
+          </Col>
+        </Row>
+      </div>
     </>
   );
 };
--- app/src/component/user/Signup/signup.js
+++ app/src/component/user/Signup/signup.js
@@ -3,6 +3,7 @@ import { Form, Field } from "react-final-form";
 import { useDispatch } from "react-redux";
 import { registerAction } from "../../../Redux/action/registerAction";
 import { Link, useNavigate } from "react-router-dom";
+import { Col, Row } from "react-bootstrap";
 
 const SignUp = () => {
   const dispatch = useDispatch();
@@ -41,86 +42,113 @@ const SignUp = () => {
   };
 
   return (
-    <div>
-      <Form
-        onSubmit={onSubmit}
-        validate={validates}
-        initialValues={initialValues}
-        render={({ handleSubmit, form, submitting, pristine, values }) => (
-          <form onSubmit={handleSubmit}>
-            <Field name="username">
-              {({ input, meta }) => (
-                <div className="mb-4">
-                  {/* <label>Your name</label> */}
-                  <input
-                    className="login_input"
-                    {...input}
-                    type="text"
-                    placeholder="First and last name"
-                  />
-                  {meta.error && meta.touched && (
-                    <p className="star">{meta.error}</p>
+    <div className="container signin_main">
+      <Row>
+        <Col md={2}></Col>
+        <Col md={8} className="main_page">
+          <Row>
+            <Col md={6} className="signup">
+              <div className="left_content">
+                <h2>Looks Like You're new here!</h2>
+                <p>sign up with your mobile number to get started</p>
+                <img
+                  src="https://freepngimg.com/thumb/categories/1342.png"
+                  alt=""
+                />
+              </div>
+            </Col>
+            <Col md={6}>
+              <div className="right_content">
+                <Form
+                  onSubmit={onSubmit}
+                  validate={validates}
+                  initialValues={initialValues}
+                  render={({
+                    handleSubmit,
+                    form,
+                    submitting,
+                    pristine,
+                    values,
+                  }) => (
+                    <form onSubmit={handleSubmit}>
+                      <Field name="username">
+                        {({ input, meta }) => (
+                          <div className="mb-4">
+                            {/* <label>Your name</label> */}
+                            <input
+                              className="login_input"
+                              {...input}
+                              type="text"
+                              placeholder="First and last name"
+                            />
+                            {meta.error && meta.touched && (
+                              <p className="star">{meta.error}</p>
+                            )}
+                          </div>
+                        )}
+                      </Field>
+                      <Field name="email">
+                        {({ input, meta }) => (
+                          <div className="mb-4">
+                            {/* <label>Email</label> */}
+                            <input
+                              className="login_input"
+                              {...input}
+                              type="email"
+                              placeholder="Email"
+                            />
+                            {meta.error && meta.touched && (
+                              <p className="star">{meta.error}</p>
+                            )}
+                          </div>
+                        )}
+                      </Field>
+                      <Field name="password">
+                        {({ input, meta }) => (
+                          <div className="mb-4">
+                            {/* <label>Password</label> */}
+                            <input
+                              className="login_input"
+                              {...input}
+                              type="password"
+                              placeholder="Password"
+                            />
+                            {meta.error && meta.touched && (
+                              <p className="star">{meta.error}</p>
+                            )}
+                          </div>
+                        )}
+                      </Field>
+                      <Field name="confirmpassword">
+                        {({ input, meta }) => (
+                          <div className="mb-4">
+                            {/* <label>Confirm</label> */}
+                            <input
+                              className="login_input"
+                              {...input}
+                              type="password"
+                              placeholder="Confirm Password"
+                            />
+                            {meta.error && meta.touched && (
+                              <p className="star">{meta.error}</p>
+                            )}
+                          </div>
+                        )}
+                      </Field>
+                      <div className="button_div">
+                        <button className="des-but" type="submit">Submit</button>
+                        <Link className="reset_button" to="/signin">
+                          Already have an Account
+                        </Link>
+                      </div>
+                    </form>
                   )}
-                </div>
-              )}
-            </Field>
-            <Field name="email">
-              {({ input, meta }) => (
-                <div className="mb-4">
-                  {/* <label>Email</label> */}
-                  <input
-                    className="login_input"
-                    {...input}
-                    type="email"
-                    placeholder="Email"
-                  />
-                  {meta.error && meta.touched && (
-                    <p className="star">{meta.error}</p>
-                  )}
-                </div>
-              )}
-            </Field>
-            <Field name="password">
-              {({ input, meta }) => (
-                <div className="mb-4">
-                  {/* <label>Password</label> */}
-                  <input
-                    className="login_input"
-                    {...input}
-                    type="password"
-                    placeholder="Password"
-                  />
-                  {meta.error && meta.touched && (
-                    <p className="star">{meta.error}</p>
-                  )}
-                </div>
-              )}
-            </Field>
-            <Field name="confirmpassword">
-              {({ input, meta }) => (
-                <div className="mb-4">
-                  {/* <label>Confirm</label> */}
-                  <input
-                    className="login_input"
-                    {...input}
-                    type="password"
-                    placeholder="Confirm Password"
-                  />
-                  {meta.error && meta.touched && (
-                    <p className="star">{meta.error}</p>
-                  )}
-                </div>
-              )}
-            </Field>
-            <div className="buttons">
-              <button type="submit">Submit</button>
-              <Link className="reset_button" to="/signin">
-                Already have an Account
-              </Link>
-            </div>
-          </form>
-        )}
-      />
+                />
+              </div>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
     </div>
   );
 };
