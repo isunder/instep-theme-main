457fff754cd7cc2ae242c6c7abf5f1e8ae71610c
noutiyal <123543321+noutiyal@users.noreply.github.com>
2023-10-05 12:44:21 +0530

    slider url add done in api

--- api/models/slider.js
+++ api/models/slider.js
@@ -1,17 +1,20 @@
 
-const mongoose=require("mongoose")
+const mongoose = require("mongoose")
 
-const sliderSchema=new mongoose.Schema({
-  name: [{
-    type:String,
-  }],
-    images: [{
-        type:String,
-      }],
+const sliderSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  url: {
+    type: String,
+  },
+  images: [{
+    type: String,
+  }]
 
 })
 
 
-sliderSchema.set("slider",true);
-const slidertable=mongoose.model("slidertable",sliderSchema)
-module.exports=slidertable
+sliderSchema.set("slider", true);
+const slidertable = mongoose.model("slidertable", sliderSchema)
+module.exports = slidertable
