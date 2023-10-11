64a81f5bc2a15ce9bf6bacf89943fad96aadf0be
amitSharma0071 <amitsh0894@gmail.com>
2023-10-11 16:30:54 +0530

    filter setup

--- api/router/BrandRought.js
+++ api/router/BrandRought.js
@@ -1,13 +1,10 @@
-
 const express = require("express");
 const router = express.Router();
 
-const  brandController  = require("../controller/brandController");
+const brandController = require("../controller/brandController");
 
 router.post("/addbrand", brandController.create_brand);
 router.get("/getOnlybrand", brandController.brandgetdata);
 router.post("/Deletebrand", brandController.deletebrand);
-
-
 
 module.exports = router;
