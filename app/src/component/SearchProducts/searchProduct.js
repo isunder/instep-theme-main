1c861500a9b1ff1813f5ca86a3f07ed451c6d0cf
InStep Technologies <code@insteptechnologies.com>
2023-12-04 12:35:26 +0530

    search product , csss

--- app/src/component/SearchProducts/searchProduct.js
+++ app/src/component/SearchProducts/searchProduct.js
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 import Subcaregoryfilter from "../user/filterbyCategory/SubcaregoryMobilefilter";
 import SubCategoryfilter from "../user/filterbyCategory/subCategoryfilter";
 import { BsFillHeartFill } from "react-icons/bs";
+import { MdScreenSearchDesktop } from "react-icons/md";
 
 function Searchproduct() {
   const item = useSelector((state) => state?.Searchproduct?.listdata);
@@ -18,56 +19,66 @@ function Searchproduct() {
             <SubCategoryfilter />
           </Col>
           <Col lg={10} md={9} sm={8}>
-            {" "}
             <Row>
-              {item.map((item) => {
-                return (
-                  <>
-                    <Col lg={3}>
-                      <Link
-                        className="card_deco"
-                        to={`/productdetail/${item._id}`}
-                      >
-                        <Card className=" forcatcards_htwd">
-                        <div className="d-flex justify-content-end mt-2 mx-2">
-                            <BsFillHeartFill
-                              style={{ color: "#808080" }}
-                              // onClick={showwishilist}
-                            />
-                          </div>
-                          <div className="img_div">
-                            <Card.Img
-                              variant="top"
-                              src={
-                                item?.image
-                                  ? item?.image
-                                  : item?.thumbnail.split(":").length > 1
-                                  ? item?.thumbnail
-                                  : `http://localhost:5000/uploads/${item.thumbnail}`
-                              }
-                            />
-                          </div>
-                          <Card.Body>
-                            <div className="item_rating">
-                              {/* <p className="homerating_cat"> {item?.rating}</p> */}
-                              {/* <p className="homerating_cat"> {item.category.category}</p> */}
+              {item.length > 0 ? (
+                item.map((item) => {
+                  return (
+                    <>
+                      <Col lg={3}>
+                        <Link
+                          className="card_deco"
+                          to={`/productdetail/${item._id}`}
+                        >
+                          <Card className=" forcatcards_htwd">
+                            <div className="d-flex justify-content-end mt-2 mx-2">
+                              <BsFillHeartFill
+                                style={{ color: "#808080" }}
+                                // onClick={showwishilist}
+                              />
                             </div>
-                            <Card.Title className="crad_text">
-                              {item.title}
-                            </Card.Title>
-                            <Card.Text className="crad_text">
-                              {item?.description}
-                            </Card.Text>
-                            <Card.Text className="crad_text">
-                              <h5> ₹ {item?.price}</h5>
-                            </Card.Text>
-                          </Card.Body>
-                        </Card>
-                      </Link>
-                    </Col>
-                  </>
-                );
-              })}
+                            <div className="img_div">
+                              <Card.Img
+                                variant="top"
+                                src={
+                                  item?.image
+                                    ? item?.image
+                                    : item?.thumbnail.split(":").length > 1
+                                    ? item?.thumbnail
+                                    : `http://localhost:5000/uploads/${item.thumbnail}`
+                                }
+                              />
+                            </div>
+                            <Card.Body>
+                              <div className="item_rating"></div>
+                              <Card.Title className="crad_text">
+                                {item.title}
+                              </Card.Title>
+                              <Card.Text className="crad_text">
+                                {item?.description}
+                              </Card.Text>
+                              <Card.Text className="crad_text">
+                                <h5> ₹ {item?.price}</h5>
+                              </Card.Text>
+                            </Card.Body>
+                          </Card>
+                        </Link>
+                      </Col>
+                    </>
+                  );
+                })
+              ) : (
+                <>
+                  <div className="d-flex justify-content-center align-items-center">
+                    <MdScreenSearchDesktop className="notfound-icon" />
+                  </div>
+                  <div className="d-flex justify-content-center align-items-center">
+                    <h1> Sorry, no result found</h1>
+                  </div>
+                  <div className="d-flex justify-content-center align-items-center">
+                    <h3> Check the spellings or try something else</h3>
+                  </div>
+                </>
+              )}
             </Row>
           </Col>
         </Row>
